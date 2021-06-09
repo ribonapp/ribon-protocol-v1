94736ff3f753772dadaa6c51a482f68be061ddcf
Juju <juju@ribon.io>
2021-06-09 18:10:41 -0300

    Add donation

--- contracts/Ribon.sol
+++ contracts/Ribon.sol
@@ -8,20 +8,55 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Ribon {
     using SafeERC20 for IERC20;
-    IERC20 public stableToken;
+    IERC20 public donationToken;
 
     uint256 public donationPoolBalance;
+    mapping(address => uint256) public integrationBalance;
+    mapping(address => uint256) public userBalance;
 
-    constructor(address _stableToken) {
-        stableToken = IERC20(_stableToken);
+    constructor(address _donationToken) {
+        donationToken = IERC20(_donationToken);
     }
 
     function getDonationPoolBalance() public view returns (uint256) {
         return donationPoolBalance;
     }
 
+    function getIntegrationBalance(address _integration)
+        public
+        view
+        returns (uint256)
+    {
+        return integrationBalance[_integration];
+    }
+
+    function getUserBalance(address _user) public view returns (uint256) {
+        return userBalance[_user];
+    }
+
     function deposit(uint256 _amount) public {
-        stableToken.safeTransferFrom(msg.sender, address(this), _amount);
+        donationToken.safeTransferFrom(msg.sender, address(this), _amount);
         donationPoolBalance = donationPoolBalance + _amount;
+    }
+
+    function allowIntegrationDistribute(address _integration, uint256 _amount)
+        public
+    {
+        donationPoolBalance = donationPoolBalance - _amount;
+        integrationBalance[_integration] =
+            integrationBalance[_integration] +
+            _amount;
+    }
+
+    function allowUserDonate(address _user, uint256 _amount) public {
+        userBalance[_user] = userBalance[_user] + _amount;
+        integrationBalance[msg.sender] =
+            integrationBalance[msg.sender] -
+            _amount;
+    }
+
+    function donate(address _nonProfit, uint256 _amount) public {
+        userBalance[msg.sender] = userBalance[msg.sender] - _amount;
+        donationToken.safeTransfer(_nonProfit, _amount);
     }
 }
--- test/ribon.js
+++ test/ribon.js
@@ -20,12 +20,90 @@ contract("Ribon", accounts => {
       await ribon.deposit(amount)
     });
       
-    it("should add the amount on total donation pool balance", async () => {
+    it("should add the amount on donation pool balance", async () => {
       const donationPoolBalance = await ribon.getDonationPoolBalance();
       assert.equal(
           donationPoolBalance.toString(),
           amount,
           "Balance wasn't 1 ether"
+        )
+    });
+  });
+
+  describe("#allowIntegrationDistribute when allow correctly ", async () => {
+    const amount = web3.utils.toWei("1", "ether");
+    
+    before(async function () {
+      await ribon.allowIntegrationDistribute(accounts[0], amount)
+    });
+      
+    it("should add the amount on integration balance", async () => {
+      const integrationBalance = await ribon.getIntegrationBalance(accounts[0]);
+      assert.equal(
+          integrationBalance.toString(),
+          amount,
+          "Balance wasn't 1 ether"
+        )
+    });
+
+    it("should remove the amount on donation pool balance", async () => {
+      const donationPoolBalance = await ribon.getDonationPoolBalance();
+      assert.equal(
+          donationPoolBalance.toString(),
+          0,
+          "Balance wasn't 0 ether"
+        )
+    });
+  });
+
+  describe("#allowUserDonate when allow correctly ", async () => {
+    const amount = web3.utils.toWei("1", "ether");
+    
+    before(async function () {
+      await ribon.allowUserDonate(accounts[0], amount)
+    });
+      
+    it("should add the amount on user balance", async () => {
+      const userBalance = await ribon.getUserBalance(accounts[0]);
+      assert.equal(
+        userBalance.toString(),
+          amount,
+          "Balance wasn't 1 ether"
+        )
+    });
+
+    it("should remove the amount on integration balance", async () => {
+      const integrationBalance = await ribon.getIntegrationBalance(accounts[0]);
+      assert.equal(
+          integrationBalance.toString(),
+          0,
+          "Balance wasn't 0 ether"
+        )
+    });
+  });
+
+  describe("#donate when donate correctly ", async () => {
+    const amount = web3.utils.toWei("1", "ether");
+    
+    before(async function () {
+      await ribon.donate(accounts[3], amount)
+    });
+      
+    it("should add the amount on non profit balance", async () => {
+      const userBalance = await ribonGov.balanceOf(accounts[3]);
+      assert.equal(
+        userBalance.toString(),
+          amount,
+          "Balance wasn't 1 ether"
+        )
+    });
+
+    it("should remove the amount on user balance", async () => {
+      const userBalance = await ribon.getUserBalance(accounts[0]);
+      assert.equal(
+          userBalance.toString(),
+          0,
+          "Balance wasn't 0 ether"
         )
     });
   });
