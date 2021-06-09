72064f1b42e7006b477d783245017f5f0bca264f
Juju <juju@ribon.io>
2021-06-09 11:22:02 -0300

    Add deposit method

--- contracts/Ribon.sol
+++ contracts/Ribon.sol
@@ -6,122 +6,22 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Ribon is ERC20 {
+contract Ribon {
     using SafeERC20 for IERC20;
+    IERC20 public stableToken;
 
-    IERC20 public governanceToken;
+    uint256 public donationPoolBalance;
 
-    address[] public integrationStakers;
-    uint256 public totalStakedByIntegrations;
-
-    mapping(address => bool) public integrationIsStaking;
-    mapping(address => bool) public integrationHasStaked;
-
-    mapping(address => uint256) public integrationStakingBalance;
-
-    mapping(address => uint256) public integrationBalance;
-    mapping(address => uint256) public userBalance;
-
-    constructor(address _governanceToken) ERC20("Ribon", "RBN") {
-        governanceToken = IERC20(_governanceToken);
+    constructor(address _stableToken) {
+        stableToken = IERC20(_stableToken);
     }
 
-    function getIntegrationStakers() public view returns (address[] memory) {
-        return integrationStakers;
-    }
-
-    function getTotalStakedByIntegrations() public view returns (uint256) {
-        return totalStakedByIntegrations;
-    }
-
-    function getIntegrationStakingBalance(address _integrationAddress)
-        public
-        view
-        returns (uint256)
-    {
-        return integrationStakingBalance[_integrationAddress];
-    }
-
-    function getIntegrationBalance(address _integrationAddress)
-        public
-        view
-        returns (uint256)
-    {
-        return integrationBalance[_integrationAddress];
-    }
-
-    function getIntegrationIsStaking(address _integrationAddress)
-        public
-        view
-        returns (bool)
-    {
-        return integrationIsStaking[_integrationAddress];
-    }
-
-    function getIntegrationHasStaked(address _integrationAddress)
-        public
-        view
-        returns (bool)
-    {
-        return integrationHasStaked[_integrationAddress];
-    }
-
-    function getUserBalance(address _userAddress)
-        public
-        view
-        returns (uint256)
-    {
-        return userBalance[_userAddress];
-    }
-
-    function stakeGovernanceTokensAsIntegration(uint256 _amount) public {
-        governanceToken.safeTransferFrom(msg.sender, address(this), _amount);
-
-        totalStakedByIntegrations = totalStakedByIntegrations + _amount;
-
-        integrationStakingBalance[msg.sender] =
-            integrationStakingBalance[msg.sender] +
-            _amount;
-
-        if (!integrationHasStaked[msg.sender]) {
-            integrationHasStaked[msg.sender] = true;
-            integrationIsStaking[msg.sender] = true;
-            integrationStakers.push(msg.sender);
-        }
-    }
-
-    function unstakeGovernanceTokensAsIntegration(uint256 _amount) public {
-        governanceToken.safeTransfer(msg.sender, _amount);
-
-        integrationStakingBalance[msg.sender] =
-            integrationStakingBalance[msg.sender] -
-            _amount;
-
-        totalStakedByIntegrations = totalStakedByIntegrations - _amount;
-
-        if (integrationStakingBalance[msg.sender] == 0) {
-            integrationIsStaking[msg.sender] = false;
-        }
+    function getDonationPoolBalance() public view returns (uint256) {
+        return donationPoolBalance;
     }
 
     function deposit(uint256 _amount) public {
-        governanceToken.safeTransferFrom(msg.sender, address(this), _amount);
-
-        for (uint256 i = 0; i < integrationStakers.length; i++) {
-            address recipient = integrationStakers[i];
-            if (integrationStakingBalance[recipient] > 0) {
-                uint256 percentage =
-                    integrationStakingBalance[recipient] /
-                        totalStakedByIntegrations;
-                uint256 numberOfTokens = _amount * percentage;
-
-                _mint(recipient, numberOfTokens);
-            }
-        }
-    }
-
-    function donate(address _ngo, uint256 _amount) public {
-        _burn(msg.sender, _amount);
-        governanceToken.safeTransfer(_ngo, _amount);
+        stableToken.safeTransferFrom(msg.sender, address(this), _amount);
+        donationPoolBalance = donationPoolBalance + _amount;
     }
 }
--- test/ribon.js
+++ test/ribon.js
@@ -10,151 +10,22 @@ contract("Ribon", accounts => {
   before(async function () {
     ribonGov = await RibonGov.deployed();
     ribon = await Ribon.deployed();
-    console.log("accounts: " + accounts[0]);
   })
-
-  it("should have Ribon name", async () => {
-    const name = await ribon.name();
-    assert.equal(
-        name,
-        "Ribon",
-        "Name wasn't Ribon"
-      )
-  });
-
-  describe("#getTotalStakedByIntegrations", async () => {      
-    it("should return total staked by integrations", async () => {
-      const totalStakedByIntegrations = await ribon.getTotalStakedByIntegrations();
-      assert.equal(
-          totalStakedByIntegrations.toString(),
-          0,
-          "Total staked should be 0"
-        )
-    });
-  });
-
-  describe("#stakeGovernanceTokensAsIntegration when stake correctly ", async () => {
-    const amount = web3.utils.toWei("1", "ether");
-    
-    before(async function () {
-      await ribonGov.approve(ribon.address, amount)
-      await ribon.stakeGovernanceTokensAsIntegration(amount)
-    });
-      
-    it("should add the amount on total staked by integrations", async () => {
-      const totalStaked = await ribon.getTotalStakedByIntegrations();
-      assert.equal(
-          totalStaked.toString(),
-          amount,
-          "Balance wasn't 1 ether"
-        )
-    });
-
-    it("should add the amount on integration staking balance", async () => {
-      const totalStaked = await ribon.getIntegrationStakingBalance(accounts[0]);
-      assert.equal(
-          totalStaked.toString(),
-          amount,
-          "Balance wasn't 1 ether"
-        )
-    });
-
-    it("should change integraitonIsStaking to true", async () => {
-      const integrationIsStaking = await ribon.getIntegrationIsStaking(accounts[0]);
-      assert.equal(
-          integrationIsStaking,
-          true,
-          "integrationIsStaking wasn't true"
-        )
-    });
-
-    it("should change integraitonHasStaked to true", async () => {
-      const integrationHasStaked = await ribon.getIntegrationHasStaked(accounts[0]);
-      assert.equal(
-          integrationHasStaked,
-          true,
-          "integrationIsStaking wasn't true"
-        )
-    });
-  });
-
-  describe("#unstakeGovernanceTokensAsIntegration when unstake correctly ", async () => {
-    const amount = web3.utils.toWei("1", "ether");
-    
-    before(async function () {
-      await ribon.unstakeGovernanceTokensAsIntegration(amount)
-    });
-      
-    it("should remove the amount on total staked by integrations", async () => {
-      const totalStaked = await ribon.getTotalStakedByIntegrations();
-      assert.equal(
-          totalStaked.toString(),
-          0,
-          "Balance wasn't 0 ether"
-        )
-    });
-
-    it("should add the amount on integration staking balance", async () => {
-      const totalStaked = await ribon.getIntegrationStakingBalance(accounts[0]);
-      assert.equal(
-          totalStaked.toString(),
-          0,
-          "Balance wasn't 0 ether"
-        )
-    });
-
-    it("should change integraitonIsStaking to false", async () => {
-      const integrationIsStaking = await ribon.getIntegrationIsStaking(accounts[0]);
-      assert.equal(
-          integrationIsStaking,
-          false,
-          "integrationIsStaking wasn't false"
-        )
-    });
-  });
 
   describe("#deposit when deposit correctly ", async () => {
     const amount = web3.utils.toWei("1", "ether");
     
     before(async function () {
       await ribonGov.approve(ribon.address, amount)
-      await ribon.stakeGovernanceTokensAsIntegration(amount)
-      await ribonGov.approve(ribon.address, amount);
-      await ribon.deposit(amount);
+      await ribon.deposit(amount)
     });
-
-    it("should distribute proportionally to integrations", async () => {
-      const balance = await ribon.balanceOf(accounts[0]);
+      
+    it("should add the amount on total donation pool balance", async () => {
+      const donationPoolBalance = await ribon.getDonationPoolBalance();
       assert.equal(
-          balance.toString(),
+          donationPoolBalance.toString(),
           amount,
           "Balance wasn't 1 ether"
-        )
-    });
-  });
-
-  describe("#donate when donate correctly ", async () => {
-    const amount = web3.utils.toWei("1", "ether");
-    
-    before(async function () {
-      await ribon.donate(accounts[1], amount)
-    });
-
-    it("should send ribon governance tokens to ngo", async () => {
-      const balance = await ribonGov.balanceOf(accounts[1]);
-      assert.equal(
-          balance.toString(),
-          amount,
-          "Balance wasn't 1 ether"
-        )
-    });
-
-    it("should burn ribon tokens", async () => {
-      const balance = await ribon.totalSupply();
-      assert.equal(
-          balance.toString(),
-          0,
-          "Total supply wasn't 0"
         )
     });
   });
